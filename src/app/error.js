7751cc170d68e5228c95807ec445b870bce09570
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-12-06 10:55:17 +0600

    Loading page error page 404 page added

---
+++ src/app/error.js
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            
+            <h2 className='text-4xl text-red-600'>Something went wrong...</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;

3d187eab40884e6db0be4edab6cc9043b4eb8b0c
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-12-06 15:46:14 +0600

    Page added

---
+++ src/app/album/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import React from 'react';
+
+const AlbumPage = () => {
+    return (
+        <div>
+            <Image src="https://i.ibb.co/1b52fNc/tour-0000-34.jpg" width={200} height={600} alt='img' />
+        </div>
+    );
+};
+
+export default AlbumPage;
